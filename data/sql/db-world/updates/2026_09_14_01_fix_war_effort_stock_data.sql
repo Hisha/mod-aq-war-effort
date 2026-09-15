39cadf85fa4bd5a4df12420c6c0c27ff96cc2864
Hisha <vndinmchine@gmail.com>
2026-09-14 20:43:40 -0400

    Quest didn't need to be repeatable

--- data/sql/db-world/updates/2026_09_14_01_fix_war_effort_stock_data.sql
+++ data/sql/db-world/updates/2026_09_14_01_fix_war_effort_stock_data.sql
@@ -9,9 +9,3 @@ WHERE gec.`eventEntry` = 22
   AND c.`id` IN (15663, 15696, 15701)
   AND c.`MovementType` = 2
   AND (ca.`path_id` IS NULL OR ca.`path_id` = 0);
-
--- Stock Horde War Effort quest 8610 "The Horde Needs More Runecloth Bandages!" is missing its repeatable flag.
-UPDATE `quest_template_addon`
-SET `SpecialFlags` = `SpecialFlags` | 1
-WHERE `ID` = 8610
-  AND (`SpecialFlags` & 1) = 0;
